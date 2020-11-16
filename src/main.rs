364b1df4f4f567a75b5ab332e9a78b7d83bd821e
Pecco <pecco@qq.com>
2020-11-16 19:02:50 +0800

    完成最初版本

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ mod details;
 use details::WithChar;
 use rand::prelude::*;
 use simple_combinators::{
-    combinator::{attempt, many1, optional},
+    combinator::optional,
     parser::{char, into_integer, spaces},
-    Parser, ParserIter,
+    ParseError, Parser,
 };
 
 enum Token {
@@ -12,6 +12,7 @@ enum Token {
     ConstantInteger(i64),
     RandomIntegerBetween(i64, i64),
     RandomIntegerNoGreaterThan(i64),
+    Repeat(usize, Vec<Token>),
 }
 impl Token {
     fn generate(&self) -> String {
@@ -22,8 +23,39 @@ impl Token {
             Token::RandomIntegerNoGreaterThan(a) => {
                 thread_rng().gen_range(0, a).to_string().with(' ')
             }
+            Token::Repeat(times, v) => {
+                let mut s = String::new();
+                for _ in 0..*times {
+                    for i in v.iter() {
+                        s.push_str(&i.generate());
+                    }
+                }
+                s
+            }
         }
     }
+}
+
+#[derive(Copy, Clone)]
+struct RepeatedToken;
+impl Parser for RepeatedToken {
+    type ParseResult = Token;
+    fn parse(&self, buf: &mut &str) -> Result<Self::ParseResult, ParseError> {
+        let times = char('x')
+            .with(into_integer())
+            .skip(spaces())
+            .skip(char('{'))
+            .parse(buf)? as usize;
+        let mut v = Vec::new();
+        for i in token().iter(buf) {
+            v.push(i);
+        }
+        char('}').parse(buf)?;
+        Ok(Token::Repeat(times, v))
+    }
+}
+fn repeated_token() -> RepeatedToken {
+    RepeatedToken
 }
 
 fn random_integer_token() -> impl Parser<ParseResult = Token> {
@@ -41,17 +73,19 @@ fn token() -> impl Parser<ParseResult = Token> {
         .with(
             random_integer_token()
                 .or(into_integer().map(|i| Token::ConstantInteger(i)))
+                .or(repeated_token())
                 .or(char('/').map(|_| Token::NewLine)),
         )
         .skip(spaces())
 }
 
 fn main() {
-    let filename = "data/template.txt";
-    let template = std::fs::read_to_string(filename).expect("failed to read");
+    let template = "data/template.txt";
+    let output = "data/output.txt";
+    let template = std::fs::read_to_string(template).expect("Failed to read");
     let mut buf = template.as_str();
     let token = token();
     let iter = token.iter(&mut buf).map(|x| x.generate());
     let s: String = iter.collect();
-    println!("{}", s);
+    std::fs::write(output, s).expect("Failed to write");
 }
