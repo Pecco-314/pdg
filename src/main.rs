6ed0829ded77c1476a9e7a8065392641637359f7
Pecco <pecco@qq.com>
2020-11-20 23:13:26 +0800

    重新组织main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,11 @@ mod token;
 use details::With;
 use parser::{file_range, token};
 use simple_combinators::Parser;
-use std::env;
-use std::fs;
-use std::io;
-use std::path::Path;
+use std::{
+    env, fs, io,
+    ops::Range,
+    path::{Path, PathBuf},
+};
 use token::{cul_token, Token};
 
 fn pause() {
@@ -18,51 +19,71 @@ fn pause() {
     io::stdin().read_line(&mut String::new()).unwrap();
 }
 
-fn main() {
+fn parse_once(mut buf: &str) -> (Range<usize>, Vec<Token>, bool) {
+    // 解析一个文件标注和其对应的模板
+    let range = file_range().parse(&mut buf);
+    let mut end = false;
+    let range = match range {
+        Ok(r) => r,
+        Err(_) => {
+            end = true;
+            1..11
+        } // 如果尚未生成过，则默认生成1.in~10.in
+    };
+    let tokens = token().iter(&mut buf).collect();
+    (range, tokens, end)
+}
+
+fn parse_and_generate(buf: &str, fold: &PathBuf) {
+    loop {
+        let (range, tokens, end) = parse_once(buf);
+        for i in range {
+            generate(i, &tokens, fold);
+        }
+        if end {
+            break;
+        }
+    }
+}
+
+fn generate(fileid: usize, tokens: &Vec<Token>, fold: &PathBuf) {
+    println!("Generating {}.in", fileid);
+    let target = fold.join(&fileid.to_string().with_str(".in"));
+    let gens = cul_token(&tokens).expect("Something went wrong while culculating tokens");
+    let mut s = String::new();
+    for i in gens.iter() {
+        s.push_str(
+            &i.generate_str()
+                .expect("Something went wrong while generating random numbers"),
+        );
+    }
+    fs::write(target, s).expect("Failed to write");
+}
+
+fn get_template<'a>() -> (PathBuf, String) {
     let args: Vec<String> = env::args().collect();
-    let template_path = if args.len() > 1 {
+    let path = if args.len() > 1 {
         &args[1]
     } else {
         "template.txt" // 默认路径
     };
-    let parent = Path::new(template_path)
-        .parent()
-        .expect("invaild path format");
-    let template = fs::read_to_string(template_path).expect("Failed to read");
-    let parent_path = parent.join("testdata");
-    fs::create_dir_all(&parent_path).expect("Failed to create directory");
-    let mut buf = template.as_str();
+    (
+        Path::new(path).to_path_buf(),
+        fs::read_to_string(path).expect("Failed to read"),
+    )
+}
 
-    let mut exit = false;
-    let mut is_first = true;
-    'out: while !exit {
-        let range = file_range().parse(&mut buf);
-        let range = match range {
-            Ok(r) => r,
-            Err(_) => {
-                if !is_first {
-                    break 'out;
-                }
-                exit = true;
-                1..11
-            } // 如果尚未生成过，则默认生成1.in~10.in并退出；否则直接退出
-        };
-        is_first = false;
-        let tokens: Vec<Token> = token().iter(&mut buf).collect();
-        println!("{:?}", tokens);
-        for i in range {
-            println!("Generating {}.in", i);
-            let target_path = parent_path.join(&i.to_string().with_str(".in"));
-            let gens = cul_token(&tokens).expect("Something went wrong while culculating tokens");
-            let mut s = String::new();
-            for i in gens.iter() {
-                s.push_str(
-                    &i.generate_str()
-                        .expect("Something went wrong while generating random numbers"),
-                );
-            }
-            fs::write(target_path, s).expect("Failed to write");
-        }
-    }
+fn get_fold(template: &PathBuf) -> PathBuf {
+    let parent = template.parent().expect("invaild path format");
+    let fold = parent.join("testdata");
+    fs::create_dir_all(&fold).expect("Failed to create directory");
+    fold
+}
+
+fn main() {
+    let (path, template) = get_template();
+    let fold = get_fold(&path);
+    let buf = template.as_str();
+    parse_and_generate(buf, &fold);
     pause();
 }
